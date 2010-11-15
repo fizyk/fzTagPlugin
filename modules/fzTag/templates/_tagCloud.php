2dc9c8ba036c16c2618be079e47169a27c9b8113
Grzegorz Śliwiński <fizyk@fizyk.net.pl>
2010-11-15 23:37:01 +0100

    fix

--- modules/fzTag/templates/_tagCloud.php
+++ modules/fzTag/templates/_tagCloud.php
@@ -1,6 +1,6 @@
 <h4><?php echo __('Tag cloud', array(), 'fzTag') ?></h4>
 <ul class="fz-tag-cloud">
 <?php foreach( $tags as $tag ): ?>
-  <li class="fz-size-<?php echo $weightMap[$tag->getCount()] ?>"><?php echo $tag ?></li>
+  <li class="fz-size-<?php echo $weightMap[$tag->getWeight()] ?>"><?php echo $tag ?></li>
 <?php endforeach; ?>
 </ul>
