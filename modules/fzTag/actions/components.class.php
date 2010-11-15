59ca4e23c65674c7fc56f96031fdaa52a3df0806
Grzegorz Śliwiński <fizyk@fizyk.net.pl>
2010-11-15 23:17:19 +0100

    updated tag Cloud to be able to show visible difference btween weights

--- modules/fzTag/actions/components.class.php
+++ modules/fzTag/actions/components.class.php
@@ -14,7 +14,46 @@ class fzTagComponents extends sfComponents
 
     public function executeTagCloud()
     {
-        $this->tags = fzTagTable::getInstance()->getTagsWeightOrderedQuery(20)->execute();
+        $this->tags = fzTagTable::getInstance()->getTagsForCloudQuery(20)->execute();
+        $weights = fzTagTable::getInstance()->getWeightsForCloudQuery(20)->execute(array(), Doctrine_Core::HYDRATE_ARRAY);
+
+        // That map will be used to create tag's style class (up to five different)
+        $this->weightMap = array();
+        // if we have only one weight, we'll just create one entry
+        if(count($weights) <= 1)
+        {
+            foreach($weights as $weight)
+            {
+                $this->weightMap[$weight['count']] = 1;
+            }
+        }
+        // if we have up to five different weights and up to five different tags,
+        // we'll just create on entry per each weight
+        elseif(count($weights) <= 5 && $this->tags->count() <= 5)
+        {
+            $css = 1;
+            foreach($weights as $weight)
+            {
+                $this->weightMap[$weight['count']] = $css++;
+            }
+        }
+        // otherwise we'll try to level that five levels between tags weight
+        else
+        {
+            $css = 1;
+            // we have five levels, so we set boundary, when each tag will change
+            $space = $this->tags->count() / 5;
+            $tags_mapped = 0;
+            foreach($weights as $weight)
+            {
+                $this->weightMap[$weight['count']] = $css;
+                // we add number of tags with current weight to know,
+                // how many of them are alrady mapped
+                $tags_mapped += $weight['num'];
+                // simply as that, if we exceed given number, we'll just get the higher number.
+                $css = floor(1 + ($tags_mapped / $space));
+            }
+        }
     }
 
 }
