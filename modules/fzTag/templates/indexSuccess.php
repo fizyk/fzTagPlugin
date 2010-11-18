65b34475a40bde19f8911b50f21a682502c9c85c
Grzegorz Śliwiński <fizyk@fizyk.net.pl>
2010-11-18 22:44:27 +0100

    paginated list of tags in fzTag/index module

--- modules/fzTag/templates/indexSuccess.php
+++ modules/fzTag/templates/indexSuccess.php
@@ -5,3 +5,18 @@
  */
 
 ?>
+<h1><?php echo __('Tags', array(), 'fzTag'); ?></h1>
+<ul class="fz-tag-list">
+<?php foreach($pager as $tag): ?>
+    <li><span class="fz-tag-name"><?php echo $tag; ?></span>
+        <span class="fz-tag-weight"><?php echo $tag->getWeight(); ?></span></li>
+<?php endforeach; ?>
+</ul>
+<div class="fz-tag-pager">
+    <?php echo link_to_if(($pager->getPage() > 1), 'Previous page',
+                'fzTag/index',
+                array('query_string' => 'page='.$pager->getPreviousPage())); ?>&nbsp;
+    <?php echo link_to_if(($pager->getPage() < $pager->getLastpage()), 'Next page',
+                'fzTag/index',
+                array('query_string' => 'page='.$pager->getNextPage())); ?>
+</div>
