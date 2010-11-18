772ad01ac4c7b671c7501aea9df4662cfa530138
Grzegorz Śliwiński <fizyk@fizyk.net.pl>
2010-11-18 22:09:46 +0100

    display weight in admin module

---
+++ modules/fzTagAdmin/templates/_reportUsage.php
@@ -0,0 +1,8 @@
+<div class="sf_admin_form_row sf_admin_text sf_admin_form_field_reportUsage">
+<div>
+<label><?php echo __('Weight') ?></label>
+<div class="content">
+    <?php echo $form->getObject()->getWeight() ?>
+</div>
+</div>
+</div>
