6ffdbb237b7b68336dfd2b500654941df0aaac71
Grzegorz Śliwiński <fizyk@fizyk.net.pl>
2010-11-18 22:19:45 +0100

    created dummy index and show actions

---
+++ modules/fzTag/actions/actions.class.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Description of fzTagActions
+ *
+ * @author fizyk
+ */
+require_once dirname(__FILE__).'/../lib/BasefzTagActions.class.php';
+class fzTagActions extends BasefzTagActions
+{
+    //put your code here
+}
+?>
---
+++ modules/fzTag/lib/BasefzTagActions.class.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Description of BasefzTagActions
+ *
+ * @author fizyk
+ */
+class BasefzTagActions extends sfActions
+{
+    public function executeIndex(sfWebRequest $request)
+    {
+        # TODO! Retrieve tag's query
+        # TODO! Create pager
+        # TODO! Show simple list
+        # TODO! Add possibility to sort by name (asc, desc) and weight (asc, weight)
+    }
+
+    public function executeShow(sfWebRequest $request)
+    {
+        # TODO! Simple tag's show action with just a header
+    }
+}
+?>
