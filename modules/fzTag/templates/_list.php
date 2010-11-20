5c3dfa37c45a7f1531735f105bab292dcba0c063
Grzegorz Śliwiński <fizyk@fizyk.net.pl>
2010-11-21 00:42:31 +0100

    tag links in components

--- modules/fzTag/templates/_list.php
+++ modules/fzTag/templates/_list.php
@@ -1,6 +1,6 @@
 <h4><?php echo __('Tags', array(), 'fzTag') ?></h4>
 <ul>
 <?php foreach( $tags as $tag ): ?>
-  <li><?php echo $tag ?></li>
+  <li><?php echo link_to( $tag, 'fz_tag_show', $tag) ?></li>
 <?php endforeach; ?>
 </ul>
--- modules/fzTag/templates/_tagCloud.php
+++ modules/fzTag/templates/_tagCloud.php
@@ -2,6 +2,8 @@
 <h4><?php echo __('Tag cloud', array(), 'fzTag') ?></h4>
 <ul class="fz-tag-cloud">
 <?php foreach( $tags as $tag ): ?>
-  <li class="fz-size-<?php echo $weightMap[$tag->getWeight()] ?>"><?php echo $tag ?></li>
+  <li class="fz-size-<?php echo $weightMap[$tag->getWeight()] ?>">
+          <?php echo link_to( $tag, 'fz_tag_show', $tag) ?>
+  </li>
 <?php endforeach; ?>
 </ul>
