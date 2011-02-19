c229802274d3ce6cf6e841fd6decdae6f8aa7a3d
Grzegorz Śliwiński <fizyk@fizyk.net.pl>
2011-02-19 13:35:35 +0100

    temporary fixes

--- modules/fzTag/templates/_3dTagCloud.php
+++ modules/fzTag/templates/_3dTagCloud.php
@@ -2,7 +2,7 @@
 <?php use_javascript('../fzTagPlugin/js/jquery.tagsphere.min.js'); ?>
 <h4><?php echo __('Tag cloud', array(), 'fzTag') ?></h4>
 <div class="fz-tag-cloud-3d" id="fz-tag-cloud-3d<?php echo $cloudOptions['cloud_id']; ?>" style="width: <?php echo $cloudOptions['width']; ?>px;
-                                    height: <?php echo $cloudOptions['height']; ?>px">
+                                    height: <?php echo $cloudOptions['height']; ?>px;">
     <ul class="fz-tag-cloud">
         <?php foreach( $tags as $tag ): ?>
             <li class="fz-size-<?php echo $weightMap[$tag->getWeight()] ?>">
@@ -15,17 +15,21 @@
     </ul>
 </div>
 <script type="text/javascript">
-    $('#fz-tag-cloud-3d<?php echo $cloudOptions['cloud_id']; ?>').tagcloud({
-        centrex: <?php echo ($cloudOptions['width']/2); ?>,
-        centrey: <?php echo ($cloudOptions['height']/2); ?>,
-        min_font_size: <?php echo $cloudOptions['min_font_size']; ?>,
-        max_font_size: <?php echo $cloudOptions['max_font_size']; ?>,
-        zoom: <?php echo $cloudOptions['zoom']; ?>,
-        min_zoom: <?php echo $cloudOptions['min_zoom']; ?>,
-        max_zoom: <?php echo $cloudOptions['max_zoom']; ?>,
-        zoom_factor: <?php echo $cloudOptions['zoom_factor']; ?>,
-        rotate_factor: <?php echo $cloudOptions['rotate_factor']; ?>,
-        fps: <?php echo $cloudOptions['fps']; ?>,
-        random_points: <?php echo $cloudOptions['random_points']; ?>
+    $(document).ready(function()
+    {
+        $('#fz-tag-cloud-3d<?php echo $cloudOptions['cloud_id']; ?>').tagcloud({
+            centrex: <?php echo ($cloudOptions['width']/2); ?>,
+            centrey: <?php echo ($cloudOptions['height']/2); ?>,
+            min_font_size: <?php echo $cloudOptions['min_font_size']; ?>,
+            max_font_size: <?php echo $cloudOptions['max_font_size']; ?>,
+            zoom: <?php echo $cloudOptions['zoom']; ?>,
+            min_zoom: <?php echo $cloudOptions['min_zoom']; ?>,
+            max_zoom: <?php echo $cloudOptions['max_zoom']; ?>,
+            zoom_factor: <?php echo $cloudOptions['zoom_factor']; ?>,
+            rotate_factor: <?php echo $cloudOptions['rotate_factor']; ?>,
+            fps: <?php echo $cloudOptions['fps']; ?>,
+            random_points: <?php echo $cloudOptions['random_points']; ?>
+        });
+        alert('Alert? I am working');
     });
 </script>
